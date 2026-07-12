e729feefc53bad77082e50d8d01f4342d7987ce3
Dog foot ruler <3510343+dogfootruler-kr@users.noreply.github.com>
2026-07-12 13:32:39 +0900

    Change base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://timothypholmes.github.io/startup-page/",
+  base: "https://dogfootruler-kr.github.io/startup-page/",
   plugins: [
     react(),
   ],
